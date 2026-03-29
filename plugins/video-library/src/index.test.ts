e2feef3188d833083b9428c99a9f23c54cd9db38
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2026-03-30 00:21:26 +0100

    refactor video-library registration test to shared contract

--- plugins/video-library/src/index.test.ts
+++ plugins/video-library/src/index.test.ts
@@ -1,23 +1,10 @@
-import assert from 'node:assert/strict';
-import test from 'node:test';
+import { testPluginRegistrationContract } from '../../test-plugin-registration-contract';
 
 import { initPlugin } from './index';
 
-test('initPlugin registers video-library ids exactly once', () => {
-  const registerCalls: string[] = [];
-  const registerComponentCalls: string[] = [];
-
-  initPlugin({
-    register: (extensionId) => {
-      registerCalls.push(extensionId);
-    },
-    registerPluginComponent: (componentId) => {
-      registerComponentCalls.push(componentId);
-    },
-  });
-
-  assert.equal(registerCalls.length, 1);
-  assert.equal(registerComponentCalls.length, 1);
-  assert.deepEqual(registerCalls, ['video-library-dashboard-tab']);
-  assert.deepEqual(registerComponentCalls, ['video_library']);
+testPluginRegistrationContract({
+  pluginId: 'video-library',
+  dashboardExtensionId: 'video-library-dashboard-tab',
+  componentId: 'video_library',
+  initPlugin,
 });
